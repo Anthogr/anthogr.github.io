6fc1514935ead148d1b922e1790822b37b9d4bd6
Anthogr <87010226+Anthogr@users.noreply.github.com>
2025-07-09 12:51:41 +0200

    Initial commit

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,10 +1,31 @@
 export const experiences = [
 	{
-		company: '',
-		time: '',
-		title: '',
-		location: '',
-		description: '',
+		company: 'Centre de recherche et d’enseignement des géosciences de l’environnement (CEREGE)',
+		time: '2024 - 2026',
+		title: 'Instrumentation Design Engineer',
+		location: 'Aix en Provence, France',
+		description: 'A*MIDEX project MicroTree',
+	},
+	{
+		company: 'Centre de recherche et d’enseignement des géosciences de l’environnement (CEREGE)',
+		time: '2021 - 2023',
+		title: 'Engineer in Data Science and Numerical Modeling',
+		location: 'Aix en Provence, France',
+		description: 'Research contract with TOTAL, Paleoclimatic model for source rock prediction',
+	},
+	{
+		company: 'Meditarranean Institute of Oceanography (MIO)',
+		time: '2018 - 2021',
+		title: 'Engineer in Data Science and Project Management',
+		location: 'Toulon, France',
+		description: 'European interreg marittimo project SICOMAR+ (Système transfrontalier pour la sécurité en mer COntre les risques de la navigation et pour la sauvegarde de l’environnement MARin)',
+	},
+	{
+		company: 'Meditarranean Institute of Oceanography (MIO)',
+		time: '2017 - 2018',
+		title: 'Engineer in Data Science and Numerical Modeling',
+		location: 'Toulon, France',
+		description: 'Research contract OSCAR (Operational drift model for SearCh And Rescue at sea)',
 	},
 	// {
 	// 	company: 'Radium Institute (Institut du Radium)',
@@ -17,10 +38,17 @@ export const experiences = [
 
 export const education = [
 	{
-		school: '',
-		time: '',
-		degree: '',
-		location: '',
+		school: 'University of Toulon',
+		time: '2013 - 2015',
+		degree: 'Master of Physical Oceanography',
+		location: 'Toulon, France',
+		description: '',
+	},
+	{
+		school: 'University of Toulon',
+		time: '2010 - 2013',
+		degree: 'Licence of Mathematics',
+		location: 'Toulon, France',
 		description: '',
 	},
 	// {
@@ -34,8 +62,24 @@ export const education = [
 
 export const skills = [
 	{
-		title: '',
-		description: '',
+		title: 'Programaming Languages',
+		description: 'Python (advanced), C# (intermediate), JAVA (beginner), Matlab (intermediate), Ferret (intermediate), Fortran (beginner)',
+	},
+	{
+		title: 'Development Tools',
+		description: 'VSCode, GitHub, GitLab, Docker, Portainer',
+	},
+	{
+		title: 'Database',
+		description: 'MySQL, MongoDB',
+	},
+	{
+		title: 'Website',
+		description: 'Astro, Wordpress',
+	},
+	{
+		title: 'Numerical modeling',
+		description: 'OGCM (NEMO, MITgcm), Lagrangian simulation (CMS, GNOME)',
 	},
 	// {
 	// 	title: 'Experimental Techniques',
@@ -45,11 +89,51 @@ export const skills = [
 
 export const publications = [
 	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
+		title: 'A Model Based Study of the Emergence of North Atlantic Deep Water During the Cenozoic: A Tale of Geological and Climatic Forcings.',
+		authors: 'Pineau, E., Donnadieu, Y., Maffre, P., Lique, C., Huck, T., Gramoullé, A., & Ladant, J.-B.',
+		journal: 'Paleoceanography and Paleoclimatology',
+		time: '2025',
+		link: 'https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2024PA005020',
+		abstract: '',
+	},
+		{
+		title: 'Impact of Dust and Temperature on Primary Productivity in Late Miocene Oceans.',
+		authors: 'Pillot, Q., Sarr, A. ‐C., Donnadieu, Y., Gramoullé, A. & Suchéras‐Marx, B.',
+		journal: 'Paleoceanography and Paleoclimatology',
+		time: '2025',
+		link: 'https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/2023PA004838',
+		abstract: '',
+	},
+		{
+		title: 'Dynamics and transport from the boundary Northern Current toward the Toulon Bay: multi-platform observations and downscaling modelling approaches.',
+		authors: 'Molcard, A., Gramoullé, A., Mazoyer, C., Bourg, N. & Ourmières, Y.',
+		journal: 'Ocean Dynamics',
+		time: '2021',
+		link: 'https://link.springer.com/article/10.1007/s10236-021-01479-4',
+		abstract: '',
+	},
+		{
+		title: 'High-Frequency Radar Measurements with CODAR in the Region of Nice: Improved Calibration and Performance.',
+		authors: 'Guérin, C., Dumas, D., Molcard, A., Quentin, C., Zakardjian, B., Gramoullé, A., & Berta, M.',
+		journal: 'Atmospheric and Oceanic Technology',
+		time: '2021',
+		link: 'https://journals.ametsoc.org/view/journals/atot/38/11/JTECH-D-21-0058.1.xml',
+		abstract: '',
+	},
+	{
+		title: 'Multistatic estimation of high-frequency radar surface currents in the region of Toulon.',
+		authors: 'Dumas, D., Gramoullé, A., Guérin, CA. et al.',
+		journal: 'Ocean Dynamics',
+		time: '2020',
+		link: 'https://link.springer.com/article/10.1007/s10236-020-01406-z',
+		abstract: '',
+	},
+		{
+		title: 'Characterizing frontal eddies along the East Australian Current from HF radar observations.',
+		authors: 'Schaeffer, A., Gramoullé, A., Roughan, M. & Mantovanelli, A.',
+		journal: 'JGR Oceans',
+		time: '2017',
+		link: 'https://agupubs.onlinelibrary.wiley.com/doi/full/10.1002/2016JC012171',
 		abstract: '',
 	},
 	// {
--- src/settings.ts
+++ src/settings.ts
@@ -1,21 +1,24 @@
 export const profile = {
-	fullName: 'Your Name',
+	fullName: 'Anthony Gramoullé',
 	title: '',
 	institute: '',
-	author_name: '', // Author name to be highlighted in the papers section
+	author_name: 'Gramoullé', // Author name to be highlighted in the papers section
 	research_areas: [
-		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
+		{ title: 'Continuous integration, continuous deployment', description: 'Design of development and deployment pipelines for Python-based web applications. Version control management (GitHub/GitLab), containerization (Docker), and deployment (Portainer).', field: 'physics' },
+		{ title: 'Hardware-software interfacing', description: 'Development of human-machine interface (HMI) control software in C#, using Modbus TCP communication with programmable logic controllers (PLCs) for machine control.', field: 'physics' },
+		{ title: 'Instrument development', description: 'Design and implementation of automated imaging instruments tailored to scientific needs. Operational follow-up of equipment: maintenance and performance optimization.', field: 'physics' },
+		{ title: 'Knowledge Transfer', description: 'Training and support in the use of developed tools, through meetings and the creation of dedicated documentation websites. Also involved in science outreach events.', field: 'physics' },
 	],
 }
 
 // Set equal to an empty string to hide the icon that you don't want to display
 export const social = {
-	email: '',
+	email: 'gramoulle@cerege.fr',
 	linkedin: '',
-	x: 'https://www.x.com/',
-	github: '',
+	x: '',
+	github: 'https://github.com/Anthogr',
 	gitlab: '',
-	scholar: '',
+	scholar: 'https://scholar.google.com/citations?user=jn_DHGkAAAAJ&hl=fr',
 	inspire: '',
 	arxiv: '',
 }
@@ -24,15 +27,15 @@ export const template = {
 	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
-	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
-	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
+	lightTheme: 'emerald', // Select one of the Daisy UI Themes or create your own
+	darkTheme: 'sunset', // Default themes are located here: https://daisyui.com/docs/themes/
 	excerptLength: 200,
 	postPerPage: 5,
     base: '' // Repository name starting with /
 }
 
 export const seo = {
-	default_title: 'Astro Academia',
+	default_title: 'Anthony Gramoullé',
 	default_description: 'Astro Academia is a template for academic websites.',
-	default_image: '/images/astro-academia.png',
+	default_image: 'favicon.png',
 }
